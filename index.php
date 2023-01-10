c83ce1798eda9d404f3263e2be85b9c18911a635
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-10 15:07:20 +0100

    Bootstrap table

--- index.php
+++ index.php
@@ -49,10 +49,67 @@ foreach ($hotels as $hotel) {
   $vote = $hotel["vote"];
   $distance = $hotel["distance_to_center"];
 
-  echo "Name: " . $name . "<br>" .
-    "Description: " . $description . "<br>" .
-    "Parking: " . $parking . "<br>" .
-    "Vote: " . $vote . "<br>" .
-    "Distance: " . $distance . "<br> <br>";
+  // echo "Name: " . $name . "<br>" .
+  //   "Description: " . $description . "<br>" .
+  //   "Parking: " . $parking . "<br>" .
+  //   "Vote: " . $vote . "<br>" .
+  //   "Distance: " . $distance . "<br> <br>";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>php-hotel</title>
+</head>
+
+<!-- 2) Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella. -->
+
+<body>
+  <div class="container mt-5">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Parking</th>
+          <th scope="col">Vote</th>
+          <th scope="col">Distance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        foreach ($hotels as $hotel) {
+          ?>
+          <tr>
+            <td>
+              <?php echo $hotel["name"] ?>
+            </td>
+            <td>
+              <?php echo $hotel["description"] ?>
+            </td>
+            <td>
+              <?php echo $hotel["parking"] ?>
+            </td>
+            <td>
+              <?php echo $hotel["vote"] ?>
+            </td>
+            <td>
+              <?php echo $hotel["distance_to_center"] ?>
+            </td>
+          </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+</body>
+
+</html>
